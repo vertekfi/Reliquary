74f6638a97a854cd5d6bbc19da58358b7484bf9b
0xBriz <chaindevone@gmail.com>
2023-05-18 14:30:57 -0400

    parent rewarder deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,8 +2,8 @@ import { parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
 
 async function main() {
-  // const rewardToken = "0x412A1ab6A00B50A7ad2306C994ae609Bd823ad87"; // The current "ARTK"(Or whatever its called then)
-
+  const rewardToken = "0x412A1ab6A00B50A7ad2306C994ae609Bd823ad87"; // The current "ARTK"(Or whatever its called then)
+  const reliquary = "0xb617d3584cbE69f04c2dEA54dF0d48876b793699";
   // const AccessControlCurve = await ethers.getContractFactory("AccessControlCurve");
   // const rate = parseUnits("1", 17);
   // console.log("rate: " + rate.toString());
@@ -23,15 +23,19 @@ async function main() {
 
   // console.log(`ReliquaryGamified deployed to: ${reliq.address}`);
 
-  const NftDescriptor = await ethers.getContractFactory("NftDescriptorMock");
-  const tokenURI = "";
-  const descriptor = await NftDescriptor.deploy(
-    "0xb617d3584cbE69f04c2dEA54dF0d48876b793699",
-    tokenURI
-  );
-  await descriptor.deployed();
+  // const NftDescriptor = await ethers.getContractFactory("NftDescriptorMock");
+  // const tokenURI = "";
+  // const descriptor = await NftDescriptor.deploy(
+  //   reliquary,
+  //   tokenURI
+  // );
+  // await descriptor.deployed();
+  // console.log(`NftDescriptor deployed to: ${descriptor.address}`);
 
-  console.log(`NftDescriptor deployed to: ${descriptor.address}`);
+  const ParentRewarder = await ethers.getContractFactory("ParentRewarder");
+  const rewarder = await ParentRewarder.deploy(parseUnits("1"), rewardToken, reliquary);
+  await rewarder.deployed();
+  console.log(`ParentRewarder deployed to: ${rewarder.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
